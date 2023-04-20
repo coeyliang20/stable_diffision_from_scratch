a3f98212fd9c9bef43e5fc892a75e510fc5775f2
Coeyliang <coeyliang20@gmail.com>
2023-04-20 05:38:14 +0000

    ** transform data

--- demo.py
+++ demo.py
@@ -27,12 +27,36 @@ config = TrainingConfig()
 config.dataset_name = "huggan/smithsonian_butterflies_subset"
 dataset = load_dataset(config.dataset_name, split="train")
 
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
-fig, axs = plt.subplots(1, 4, figsize=(16, 4))
-for i, image in enumerate(dataset[:4]["image"]):
-    axs[i].imshow(image)
-    axs[i].set_axis_off()
-# fig.show()
-# save the figure to a file instead of showing it
-fig.savefig('my_plot.png')
+# fig, axs = plt.subplots(1, 4, figsize=(16, 4))
+# for i, image in enumerate(dataset[:4]["image"]):
+#     axs[i].imshow(image)
+#     axs[i].set_axis_off()
+# # fig.show()
+# # save the figure to a file instead of showing it
+# fig.savefig('my_plot.png')
+
+import torch
+from torchvision import transforms
+
+preprocess = transforms.Compose(
+    [
+        transforms.Resize((config.image_size, config.image_size)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5], [0.5]),
+    ]
+)
+
+def transform(examples):
+    images = [preprocess(image.convert("RGB")) for image in examples["image"]]
+    return {"images": images}
+
+
+dataset.set_transform(transform)
+train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=config.train_batch_size, shuffle=True)
+
+# iterate through the data and output the shape of each item
+for batch in train_dataloader:
+    print(batch["images"].shape)
